839d8aeff08ac22ee25bb95a436475639aca9f79
Afrikhana <brianmurigi64@gmail.com>
2023-11-02 22:13:44 +0300

    :art::sparkle:Created Demo Navbar

--- elewa/src/app/app.component.ts
+++ elewa/src/app/app.component.ts
@@ -3,11 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template:` 
-  <div class='header'>
     <app-client></app-client>
-  </div> 
-
-  <app-about></app-about>
+    <app-about></app-about>
   
   `,
   styleUrls: ['./app.component.scss']
--- elewa/src/app/client/client.component.ts
+++ elewa/src/app/client/client.component.ts
@@ -3,12 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-client',
   template: `
-    <nav>
-      <a routerLink="/about" routerLinkActive="active" >{{about}}</a>
-      <a routerLink="/social" routerLinkActive="active" >{{social}}</a>
-      <a routerLink="/invest" routerLinkActive="active" >{{invest}}</a>
-    </nav>
 
+        <div class="header">
+          <div id="logo">
+            <img src={{logo}} alt="elewa-logo">
+          </div>
+        <nav>
+            <a routerLink="/about" routerLinkActive="active" >{{about}}</a>
+            <a routerLink="/social" routerLinkActive="active" >{{social}}</a>
+            <a routerLink="/invest" routerLinkActive="active" >{{invest}}</a>
+          </nav>
+        </div>
+        <div class="line-header"></div>
+   
   `,
   styleUrls: ['./client.component.scss']
 })
@@ -16,4 +23,5 @@ export class ClientComponent {
   public about = 'About';
   public social = 'Social';
   public invest = 'Invest';
+  public logo = 'https://res.cloudinary.com/ddei3mzex/image/upload/v1698922465/Logo_White_arvoyx_h9kx9y.svg'
 }
