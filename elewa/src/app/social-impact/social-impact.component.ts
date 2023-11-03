e7e0ffe2b884a8140b721de83513c1e0c926a12c
Afrikhana <brianmurigi64@gmail.com>
2023-11-03 15:14:02 +0300

    :bug::rocket:Built foundation of social page

--- elewa/src/app/social-impact/social-impact.component.ts
+++ elewa/src/app/social-impact/social-impact.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `
   <!-- contains display picture and content -->
    <div class='social-header-components'>   
+    <!-- first intro to a page display -->
               <div class='social-container'>
                   <img id='chains' src={{image}} alt="chains">
                   <div id='content'>
@@ -14,14 +15,66 @@ import { Component } from '@angular/core';
                   <div id="display-work-content">
                     <app-work-content></app-work-content>
                   </div>
-                  <div id='business-for-win'>
-                    <img src="" alt="">
+<!-- second intro on social business -->
+                  <div class='business-for-win'>
+                    <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1681306013/lady-impact_rnc9jy.jpg" alt="lady">
                     <div class="business-content">
-                      <h1>(Social) business for the win</h1>
+                      <h1>(Social) business for the <br>win</h1>
                       <p>with a young dynamic population that is eager to <br>contribution to the global economy, East Africa has a <br>
-                    lot to offer to the world. Now is the time to nature <br>sustainable, inclusive and scalable business.The rest <br>will take care of itself.</p>
+                    lot to offer to the world. Now is the time to nature sustainable,<br> inclusive and scalable business.The rest will take care of<br> itself.</p>
                     </div>
                   </div>
+<!-- third intro based on cooperation -->
+                    <div class="cop-mindset">
+                      <div id="mind-content">
+                        <h1>A cooperative mindset</h1>
+                        <p>Elewa has one shared objective; To unlock the truth <br>potential of individuals, teams, and the community. We <br>
+                        believe strongly in the power of sharing ideas and <br> continously strive to grow each other and ourselves. <br>Internally, but also withing the large communities in which <br>
+                      we are active.</p>
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1681308135/buffelo_i60xeu.jpg" alt="buffalo">
+                      </div>
+                    </div>
+<!-- forth intro based on beyond business -->
+                    <div class="beyond-business">
+                      <div id="beyond-content">
+                          <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,q_90,w_400/v1675690301/elewa-group-website/Images/IMG_0764_twyo3k.png" alt="kids">
+                          <h1>Beyond business</h1>
+                          <p>We channel a good amount of our resources into giving <br>back.
+                              <br>Through projects such as our coding school in Kakuma <br> refugee camp, training teachers in pastoral Samburu and <br>
+                              our open-source coding initiatives providing entry-level <br> opportunities to hundreds of junior engineers <br><br>These projects dont necessarily have an immediate <br>
+                              financial return, but their impact can be tangibly <br>measured in the long-term. 
+                          </p>
+                      </div>
+                    </div>
+<!-- fifth intro based on open knowledge -->
+                    <!-- <div class='open-knowledge'>
+                      <div id='open-content'>
+                        <h1>Open knowledge</h1>
+                        <p>what we learn, we share. Through community events, <br>open knowledge repositories, regular teaching moments <br>
+                        (everyone is a teacher) and academic partnerships. <br><br>We build for today, with aaaaaaa lens on tomorrow.
+                      </p>
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690302/elewa-group-website/Images/unnamed_1_m3dvll.png" alt="co-founder">
+                      </div>
+                    </div> -->
+<!-- sicth intro based on missions and companies/slideshow -->
+                    <!-- <div class='before-footer'>
+                      <h1>Some of our impact projects</h1>
+                      <div id="slide-show">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380443/elewa-group-website/client-logos/ms-adc_y96dms.jpg" alt="microsoft">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380412/elewa-group-website/client-logos/Spotlights_7_pplnvm.jpg" alt="spotlights">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380406/elewa-group-website/client-logos/Kenya_Scouts_Association_kcoka5.svg" alt="kenya=scouts">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380406/elewa-group-website/client-logos/wwf-4-logo-png-transparent_tbkfcq.png" alt="panda">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380406/elewa-group-website/client-logos/vvob-logo_qyvjbp.jpg" alt="Rwanda">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380406/elewa-group-website/client-logos/farmbetter-logo_x2acda.png" alt="farmbetter">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380405/elewa-group-website/client-logos/it_volk_black_logo_image_iowrws.png" alt="volk">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380405/elewa-group-website/client-logos/hoplit-logo_nsryy7.webp" alt="client-logo">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380405/elewa-group-website/client-logos/2017-GIZ-Logo_htdnpa.png" alt="giz">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380405/elewa-group-website/client-logos/Enabel-logo_afiel7.png" alt="Enable">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380406/elewa-group-website/client-logos/syndicyourself_logfo_qiqrex.png" alt="sydanic">
+                        <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/e_grayscale/v1681380405/elewa-group-website/client-logos/adc-logo_dgxpul.jpg" alt="africa">
+                      </div>
+                      <h1>Become part of our mission <br> and <span>join elewa.</span></h1>
+                    </div> -->
               </div>
    </div>
     <!-- <app-about-info></app-about-info> -->
